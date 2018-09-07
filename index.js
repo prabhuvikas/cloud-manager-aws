b0f98744a09c73fd3f4ab40b4e23644546368f76
Vikas <prabhuvikas@gmail.com>
2018-09-07 20:16:27 +0530

    list aws

--- index.js
+++ index.js
@@ -1,45 +1,21 @@
 var AWS = require('aws-sdk');
+// load the environment 
+require('dotenv').load();
 // Set the region 
-AWS.config.update({region: 'REGION'});
-
+AWS.config.update({region: 'ap-southeast-1'});
+console.log('The value of PORT is:', process.env.AWS_ACCESS_KEY_ID);
 // Create EC2 service object
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 var params = {
-  InstanceIds: [process.argv[3]],
-  DryRun: true
+  DryRun: false
 };
 
-if (process.argv[2].toUpperCase() === "START") {
-  // call EC2 to start the selected instances
-  ec2.startInstances(params, function(err, data) {
-    if (err && err.code === 'DryRunOperation') {
-      params.DryRun = false;
-      ec2.startInstances(params, function(err, data) {
-          if (err) {
-            console.log("Error", err);
-          } else if (data) {
-            console.log("Success", data.StartingInstances);
-          }
-      });
-    } else {
-      console.log("You don't have permission to start instances.");
-    }
-  });
-} else if (process.argv[2].toUpperCase() === "STOP") {
-  // call EC2 to activate monitoring for selected instance
-  ec2.stopInstances(params, function(err, data) {
-    if (err && err.code === 'DryRunOperation') {
-      params.DryRun = false;
-      ec2.stopInstances(params, function(err, data) {
-          if (err) {
-            console.log("Error", err);
-          } else if (data) {
-            console.log("Success", data.StoppingInstances);
-          }
-      });
-    } else {
-      console.log("You don't have permission to stop instances");
-    }
-  });
-}
+// call EC2 to retrieve policy for selected bucket
+ec2.describeInstances(params, function(err, data) {
+  if (err) {
+    console.log("Error", err.stack);
+  } else {
+    console.table("Success", data);
+  }
+});
